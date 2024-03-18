18a73b7c797c96d18ee747ca2a55556b8126b9c8
Abraham <freddy.balboa@tecsup.edu.pe>
2024-03-18 00:52:18 -0500

    1

---
+++ src/routes/clientes.routes.js
@@ -0,0 +1,24 @@
+import {Router} from 'express'
+import pool from '../database.js'
+
+const router = Router();
+
+router.get('/add', (req,res) => {
+    res.render('clientes/add');
+});
+
+router.post('/add', async(req, res) => {
+    try{
+        const {nomcli, apecli, nrodnicli, telcli} = req.body;
+        const newCliente = {
+            nomcli, apecli, nrodnicli, telcli
+        }
+        await pool.query('INSERT INTO clientes SET ?', [newCliente]);
+        res.redirect('/list');
+    }
+    catch(err){
+        res.status(500).json({message:err.message});
+    }
+});
+
+export default router;
